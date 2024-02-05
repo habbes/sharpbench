475f11ea6df020c7cc074d27e7763f9bc7b15fde
Clément Habinshuti <haby_habbes@live.com>
2024-02-05 18:55:39 +0300

    Get mirrorsharp to work

--- src/SharpbenchApi/Program.cs
+++ src/SharpbenchApi/Program.cs
@@ -3,7 +3,10 @@ using MirrorSharp.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// See: https://github.com/ashmind/mirrorsharp
+app.UseWebSockets();
 app.MapMirrorSharp("/mirrorsharp");
-app.MapGet("/", () => "Hello World!");
+
 
 app.Run();
+
--- src/sharpbench-app/src/App.tsx
+++ src/sharpbench-app/src/App.tsx
@@ -1,37 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
-import "mirrorsharp/mirrorsharp.css"
-import mirrorsharp from 'mirrorsharp-codemirror-6-preview';
+import "mirrorsharp-codemirror-6-preview/mirrorsharp.css";
+import mirrorsharp, { MirrorSharpInstance } from 'mirrorsharp-codemirror-6-preview';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const containerRef = useRef<HTMLDivElement>(null);
+  const ms = useRef<MirrorSharpInstance<void>>(null);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+    if (ms.current) return;
+    const initial = getLanguageAndCode();
+    console.log('rendering ms');
+    ms.current = mirrorsharp(
+      containerRef.current, {
+        serviceUrl: 'ws://localhost:5176/mirrorsharp',
+        language: initial.language,
+        text: initial.code,
+        serverOptions: (initial.mode !== 'regular' ? { 'x-mode': initial.mode } : {})
+      })
+  },);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div ref={containerRef}></div>
     </>
   )
+}
+
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function getCode(language: any, mode: any) {
+  if (mode === 'script') {
+      return 'var messages = Context.Messages;';
+  }
+  else if (language == 'C#') {
+      return `using System;
+
+      class C {
+          const int C2 = 5;
+          string f;
+          string P { get; set; }
+          event EventHandler e;
+          event EventHandler E { add {} remove {} }
+
+          C() {
+          }
+
+          void M(int p) {
+              var l = p;
+          }
+      }
+
+      class G<T> {
+      }`.replace(/(\r\n|\r|\n)/g, '\r\n') // Parcel changes newlines to LF
+        // eslint-disable-next-line no-regex-spaces
+        .replace(/^        /gm, '');
+  }
+  else if (language === 'F#') {
+      return '[<EntryPoint>]\r\nlet main argv = \r\n    0';
+  }
+  else if (language === 'IL') {
+      return '.class private auto ansi \'<Module>\'\r\n{\r\n}';
+  }
+}
+
+const getLanguageAndCode = () => {
+  const params = window.location.hash.replace(/^\#/, '').split('&').reduce((result, item) => {
+      const [key, value] = item.split('=');
+      // @ts-expect-error test
+      result[key] = value;
+      return result;
+  }, {});
+  // @ts-expect-error test
+  const language = (params['language'] || 'CSharp').replace('Sharp', '#');
+  // @ts-expect-error test
+  const mode = params['mode'] || 'regular';
+  const code = getCode(language, mode);
+
+  return { language, mode, code };
 }
 
 export default App
