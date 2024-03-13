5470a926692893c61539a6799c590607c2378a8b
Clément Habinshuti <haby_habbes@live.com>
2024-03-13 19:16:26 +0300

    Add comment explaining the switch from monaco

--- webapp/src/components/code-editor/code-editor.tsx
+++ webapp/src/components/code-editor/code-editor.tsx
@@ -2,6 +2,12 @@ import { MirrorSharpEditor } from "./mirrorsharp-editor";
 import { MonacoEditor } from "./monaco-editor";
 import { CodeEditorProps } from "./types";
 
+// I switched from MirrorSharp to editor to Monaco because
+// MirrorSharp was buggy to the point that it was not feasible to write
+// a complete benchmark in the editor. The cursor would jump to seemingly
+// random location, not sure what the issue is. If I figure out how to fix it,
+// I'll go back to Mirrorsharp since it has out-of-the box support for
+// C# intellisense and auto-complete
 const EditorType = 'monaco';
 
 export function CodeEditor(props: CodeEditorProps) {
