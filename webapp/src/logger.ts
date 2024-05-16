8f08698c0053bd0efe1734ed22fbcf12937211b4
Clément Habinshuti <haby_habbes@live.com>
2024-05-16 19:00:39 +0300

    Fix client dev logger env variable

---
+++ webapp/src/logger.ts
@@ -0,0 +1,11 @@
+
+
+class Logger {
+    log(message: unknown, ...args: unknown[]) {
+        if (import.meta.env.VITE_DEV_LOGGER) {
+            console.log(message, ...args);
+        }
+    }
+}
+
+export const logger = new Logger();
