ff759de0cc5aeb7136d00efd00bdc2b85564b3cf
Clément Habinshuti <haby_habbes@live.com>
2024-05-18 22:40:12 +0300

    Reduce results and logs text size

--- webapp/src/components/logs-container.tsx
+++ webapp/src/components/logs-container.tsx
@@ -2,7 +2,7 @@ import { LogMessage } from '@/types';
 
 export function LogsContainer({ logs } : { logs: LogMessage[] }) {
   return (
-    <div className="font-mono p-4 text-sm">
+    <div className="font-mono p-4 text-xs">
       {
         logs.map((message, index) => (
           <div key={index} className={message.LogSource === 'stderr' ? 'text-red-500' : ''}>
