c6d39c95b9b0b4892e2d942338cd2bcd0db0ab39
Clément Habinshuti <haby_habbes@live.com>
2024-09-18 22:07:34 +0300

    Create job messages

---
+++ core/Messages.cs
@@ -0,0 +1,32 @@
+﻿using Sharpbench.Core;
+
+namespace SharpbenchCore;
+
+public record JobMessage(string JobId, JobMessageType Type, byte[] Data);
+
+record LogMessage(string Type, string JobId, string LogSource, string Message);
+record JobCompleteMessage(string Type, string JobId, Job Job);
+
+record JobTitleGeneratedMessage(string Type, string JobId, string Title);
+
+record JobResultsAnalysisGeneratedMessage(string Type, string JobId, string Analysis);
+
+public enum JobMessageType
+{
+    /// <summary>
+    /// A log message from stdout or stderr
+    /// </summary>
+    Log,
+    /// <summary>
+    /// A status update
+    /// </summary>
+    Status,
+    /// <summary>
+    /// Notification that title was auto-generated
+    /// </summary>
+    TitleGenerated,
+    /// <summary>
+    /// Notification that analysis was auto-generated
+    /// </summary>
+    ResultsAnalysisGenerated,
+}
